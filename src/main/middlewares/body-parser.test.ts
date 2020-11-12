c09418c72abbee278711fd3667f5057ef3f62da9
Simão Neto <simaomenezesneto@gmail.com>
2020-11-12 21:48:57 +0000

    feat: ensure requests parse body as json

--- src/main/middlewares/body-parser.test.ts
+++ src/main/middlewares/body-parser.test.ts
@@ -1,12 +1,10 @@
 import request from 'supertest'
 import app from '../config/app'
 
-describe('', () => {
-
-    test('', async () => {
+describe('Body Parser Middleware', () => {
+    test('Should parse body as json', async () => {
         app.post('/test_body_parser', (req, res) => {
             res.send(req.body)
-
         })
         await request(app)
         .post('test_body_parser')
