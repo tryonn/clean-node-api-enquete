006b46da993d31b3ff6f13cdab4f1ce046aa0866
Simão Neto <simaomenezesneto@gmail.com>
2020-11-16 20:55:32 +0000

    refactor: remove test

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -22,12 +22,14 @@ export class SignUpController implements Controller{
             if(error){
                 return badRequest(error)
             }
-            const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+            
+            /*const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
             for(const filed of requiredFields){
                 if(!httpRequest.body[filed]){
                     return badRequest(new MissingParamError(filed))
                 }
             }
+            */
 
             let { name, email, password, passwordConfirmation } = httpRequest.body
 
