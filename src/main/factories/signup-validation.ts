c5fd96488fb2d48f127ff6bcbd3f4fc97d17554c
Simão Neto <simaomenezesneto@gmail.com>
2020-11-16 21:51:18 +0000

    refactor: move password comparation to the ValidationComposite

--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -1,4 +1,5 @@
 /* utilizando o padrao factory */
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation';
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation';
 import { Validation } from '../../presentation/helpers/validators/validation';
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite';
@@ -10,5 +11,6 @@ export const makeSignUpValidation = (): ValidationComposite => {
     for(const field of ['name', 'email', 'password', 'passwordConfirmation']){
         validations.push(new RequiredFieldValidation(field))            
     }
+    validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
     return new ValidationComposite(validations)
 }
--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -22,21 +22,9 @@ export class SignUpController implements Controller{
             if(error){
                 return badRequest(error)
             }
-            
-            /*const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-            for(const filed of requiredFields){
-                if(!httpRequest.body[filed]){
-                    return badRequest(new MissingParamError(filed))
-                }
-            }
-            */
 
             let { name, email, password, passwordConfirmation } = httpRequest.body
-
-            if(password !== passwordConfirmation){
-                return badRequest(new InvalidParamError('passwordConfirmation'))
-            }
-    
+            
             const isValid = this.emailValidaotr.isValid(email)
             if (!isValid){
                 return badRequest(new InvalidParamError('email'))
