22cc848afd11574ecea529b89b3c51585b7cb63a
Simão Neto <simaomenezesneto@gmail.com>
2021-01-19 11:42:15 +0000

    test: ensure DbLoadAccountByToken returns an account on success

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -78,6 +78,12 @@ describe('DbLoadAccountByToken Usecase', () => {
         expect(account).toBeNull()
     })
 
+    test('Should return an account on success', async () => {
+        const { sut } = makeSut()
+        const account = await sut.load('any_token', 'any_role')
+        expect(account).toEqual(makeFakeAccount())
+    })
+
 
 
 })
