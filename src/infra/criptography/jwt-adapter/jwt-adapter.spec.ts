b13da9664041946c8973bdab81d75ddc7da42e33
Simão Neto <simaomenezesneto@gmail.com>
2021-01-20 17:29:51 +0000

    test: ensure JwtAdapter returns a decrypted value on verify success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -44,11 +44,19 @@ describe('JWT Adapter', () => {
     })
 
     describe('verify()', () => {
+
         test('Should call verify with correct values', async () => {
             const sut = makeSut()
             const verifySpy = jest.spyOn(jwt, 'verify')
             await sut.decrypt('any_token')
             expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
+        })
+
+        test('Should return a value on verify success', async () => {
+
+            const sut = makeSut()
+            const value = await sut.decrypt('any_token')
+            expect(value).toBe('any_value')
         })
     })
 })
