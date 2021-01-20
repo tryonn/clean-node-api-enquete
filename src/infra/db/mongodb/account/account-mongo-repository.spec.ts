86ef93ed1cb4cf9104d9d184c64cf98b75f91de6
Simão Neto <simaomenezesneto@gmail.com>
2021-01-20 18:03:47 +0000

    test: ensure AccountMongoRepository find an account by token with role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -81,6 +81,7 @@ describe('Account Mongo Repository', () => {
     })
 
     describe('loadByToken', () => {
+
         test('Should return an account on loadByToken without role', async () => {
             const sut = makeSut()
             await accountCollection.insertOne({
@@ -90,6 +91,23 @@ describe('Account Mongo Repository', () => {
                 accessToken: 'any_token'
             })
             const account = await sut.loadByToken('any_token')
+            expect(account).toBeTruthy()
+            expect(account.id).toBeTruthy()
+            expect(account.name).toBe('any_name')
+            expect(account.email).toBe('any_email@mail.com')
+            expect(account.password).toBe('any_password')
+        })
+
+        test('Should return an account on loadByToken with role', async () => {
+            const sut = makeSut()
+            await accountCollection.insertOne({
+                name: 'any_name',
+                email: 'any_email@mail.com',
+                password: 'any_password',
+                accessToken: 'any_token',
+                role: 'any_role'
+            })
+            const account = await sut.loadByToken('any_token', 'any_role')
             expect(account).toBeTruthy()
             expect(account.id).toBeTruthy()
             expect(account.name).toBe('any_name')
