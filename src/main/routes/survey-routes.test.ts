4ddd596ccf25580f73b90a1e235214e131418986
Simão Neto <simaomenezesneto@gmail.com>
2021-03-01 13:56:55 +0000

    test: ensure add survey route returns 204 if valid access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,9 +2,12 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 import { Collection } from 'mongodb';
+import { sign } from 'jsonwebtoken';
+import env from '../config/env';
 
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Survey Routes', () => {
 
@@ -19,9 +22,14 @@ describe('Survey Routes', () => {
     beforeEach(async () => {
         surveyCollection = await MongoHelper.getCollection('surveys')
         await surveyCollection.deleteMany({})
+
+        accountCollection = await MongoHelper.getCollection('accounts')
+        await accountCollection.deleteMany({})
+
     })
 
     describe('Post /surveys', () => {
+
         test('Should return 403 on add survey without accessToken', async () => {
             await request(app)
             .post('/api/surveys')
@@ -36,6 +44,42 @@ describe('Survey Routes', () => {
                 }]
             })
             .expect(403)
+        })
+
+
+        test('Should return 204 on add survey with valid accessToken', async () => {
+            const res = await accountCollection.insertOne({
+                name: 'Neto',
+                email: 'smenezes@gmail.com',
+                password: '123',
+                role: 'admin'
+            })
+
+            const id = res.ops[0]._id
+            const accessToken = sign({ id }, env.jwtSecret)
+
+            await accountCollection.updateOne({
+                _id: id 
+            }, 
+            {
+                $set: {
+                    accessToken
+                }
+            })
+            await request(app)
+            .post('/api/surveys')
+            .set('x-access-token', accessToken)
+            .send({
+                question: 'Question',
+                answers: [{
+                    answer: 'Answer 1',
+                    image: 'http://image-name.com'
+                },
+                {
+                    answer: 'Answer 2'
+                }]
+            })
+            .expect(204)
         })
     })
 })
