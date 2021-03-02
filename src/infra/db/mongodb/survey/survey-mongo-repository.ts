37f1364f273fc002976cb6cf2f9e9551c7e9e7a9
Simão Neto <simaomenezesneto@gmail.com>
2021-03-02 10:45:50 +0000

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,13 +1,20 @@
+import { LoadSurveysRepository } from './../../../../data/protocols/db/survey/load-surveys-repository';
 import { MongoHelper } from './../helpers/mongo-helper';
 import { AddSurveyModel } from '../../../../domain/usecases/add-survey';
 import { AddSurveyRepository } from './../../../../data/protocols/db/survey/add-survey-repository';
+import { SurveysModel } from '../../../../domain/models/survey';
 
-export class SurveyMongoRepository implements AddSurveyRepository {
-
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
 
     async add(surveyData: AddSurveyModel): Promise<void> {
         const surveyCollection = await MongoHelper.getCollection('surveys')
         await surveyCollection.insertOne(surveyData)
+    }
+
+    async loadAll(): Promise<SurveysModel[]> {
+        const surverysCollection = await MongoHelper.getCollection('surveys')
+        const surveys: SurveysModel[] = await surverysCollection.find().toArray()
+        return surveys;
     }
 
 }
