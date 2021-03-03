953676d0c37f4e144a8aa4e7a323982466bda990
Simão Neto <simaomenezesneto@gmail.com>
2021-03-03 09:41:49 +0000

    refactor: change of test

--- src/main/adapters/express/express-route-adapter.ts
+++ src/main/adapters/express/express-route-adapter.ts
@@ -12,10 +12,10 @@ export const adaptRoute = (controller: Controller) => {
 
         const httpResponse = await controller.handle(httpRequest)
         if(httpResponse.statusCode >= 200 || httpResponse.statusCode <= 299){
-            res.status(httpResponse.statusCode).json(httpResponse)
+            res.status(httpResponse.statusCode).json(httpResponse.body)
         } else {
             res.status(httpResponse.statusCode).json({
-                error: httpResponse.body.message
+                error: httpResponse.body
             })
         }
     }
