3af322b0767b8b915ebf281cb0d4bbd2285a8667
Simão Neto <simaomenezesneto@gmail.com>
2021-03-19 18:14:38 +0000

    chore: refacor all start new structure

--- bkp-19-03-2021/jest-mongodb-config.js
+++ bkp-19-03-2021/jest-mongodb-config.js
@@ -1,10 +1,13 @@
 module.exports = {
+  mongodbMemoryServer: {
+    version: 'latest'
+  },
   mongodbMemoryServerOptions: {
     instance: {
       dbName: 'jest'
     },
     binary: {
-      version: '4.0.3',
+      version: '4.0.14',
       skipMD5: true
     },
     autoStart: false
