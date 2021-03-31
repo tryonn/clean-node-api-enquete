4746f62c4ce99a84d832c1c832a7a9bb44e65a52
Simão Neto <simaomenezesneto@gmail.com>
2021-03-31 22:34:55 +0100

    test: validation of email

---
+++ tests/infra/validators/email-validator-adapter.spec.ts
@@ -0,0 +1,22 @@
+import { EmailValidatorAdapter } from '@/infra/validators/email-validator-adapter'
+
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail (): boolean {
+    return true
+  }
+}))
+
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
+describe('EmailValidatorAdapter', () => {
+  test('Should return false if validator return false', () => {
+    const sut = makeSut()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    const isValid = sut.isValid('invalid_email@mail.com')
+    expect(isValid).toBe(false)
+  })
+})
